f406191b439cbb888b6279e01127d523142f3b60
EdwinMontero88 <edwin.montero@uhispano.ac.cr>
2025-02-25 19:48:47 -0600

    Primera fase de factura
    Primera fase de factura, falta refinar el QR y tomar el objeto de entrada generado en la selección de asientos.

--- Cine/Factura.aspx
+++ Cine/Factura.aspx
@@ -4,12 +4,39 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <link rel="stylesheet" type="text/css" href="/Content/css/template/FacturaStyle.css"/>
+    <title>Tiquete de entrada</title>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        <div class="ticket-container">
+            <div class="ticket-header">
+                <h1>Event Ticket</h1>
+                <p>Event Date: 25th Feb 2025</p>
+            </div>
+            <div class="ticket-body">
+                <div class="event-details">
+                    <h2>Event Name</h2>
+                    <p>Location: Main Hall, City Center</p>
+                    <p>Seat: A12</p>
+                    <p>Time: 7:00 PM</p>
+                </div>
+                <div class="qr-code">
+                    <asp:Image ID="qrCodeImage" runat="server" alt="QR Code" />
+                </div>
+            </div>
+            <div class="ticket-footer">
+                <p>Powered by XYZ Ticketing</p>
+            </div>
         </div>
+            <div class="dialog-container" id="divMensaje" style="display: none;" runat="server">
+                <div class="message-box">
+                    <div id="mensajeContenido">
+                        <span id="mensajeTexto" runat="server"></span>
+                        <button id="cerrarMensaje" class="btnClass btnMensaje" onclick="cerrarMensaje()">Cerrar</button>
+                    </div>
+                 </div>
+            </div>
     </form>
 </body>
 </html>
--- Cine/Factura.aspx.cs
+++ Cine/Factura.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using QRCoder;
 
 namespace Cine
 {
@@ -11,7 +14,37 @@ namespace Cine
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                string qrCodeDataUrl = GenerateQRCode("25 Feb 2025 19:00:00");
+                qrCodeImage.ImageUrl = qrCodeDataUrl;
+            }
+        }
 
+        public string GenerateQRCode(string text)
+        {
+            try
+            {
+                using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+                {
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+                    using (QRCode qrCode = new QRCode(qrCodeData))
+                    {
+                        using (Bitmap qrCodeImage = qrCode.GetGraphic(20))
+                        {
+                            string filePath = Server.MapPath("~/qr_codes/QRCode.png");
+                            qrCodeImage.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+                            return "/qr_codes/QRCode.png";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                mensajeTexto.InnerText = "Ocurrió un error: " + ex.Message;
+                divMensaje.Style["display"] = "block";
+                return string.Empty;
+            }
         }
     }
 }
