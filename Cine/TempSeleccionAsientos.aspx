7d0b7ceb5ccd2a5012cc73c88a4c213061a0646b
Kenny <113489501+killjoy2001@users.noreply.github.com>
2025-02-27 10:50:53 -0600

    Se creó un aspx únicamente con la etiqueta dinámica que se encarga de crear los asientos con base en los datos XML. El XML contiene la cantidad total de asientos, y su estado (disponible, ocupado, o seleccionado. Una función js se encarga de que cambiar los estados de los asientos y que los que estén ocupados no se puedan seleccionar.

---
+++ Cine/TempSeleccionAsientos.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TempSeleccionAsientos.aspx.cs" Inherits="Cine.TempSeleccionAsientos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" type="text/css" heref="css/asientos.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div id="asientos-container" runat="server"></div> <%--carga dinámicamente los asientos con base en el XML.--%> 
+        </div>
+    </form>
+    <script src="Scripts/js/asientos.js"></script>
+</body>
+</html>
---
+++ Cine/TempSeleccionAsientos.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace Cine
+{
+    public partial class TempSeleccionAsientos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarAsientos();
+            }
+        }
+
+        // CargarAsientos() recorre los asientos y su estado en el XML, y dentro del foreach va creando un div con un ID unico 
+        // por cada asiento.Ese div está en la línea 13 del aspx. 
+        // Vamos a necesitar crear un XML por cada película en cartelera y reutilizar el script y el estilo.  
+
+        private void CargarAsientos() 
+        {
+            string xmlPath = Server.MapPath("DataFiles/asientos.xml");
+            XDocument doc = XDocument.Load(xmlPath);
+
+            var asientos = doc.Descendants("asiento")
+                              .Select(a => new
+                              {
+                                  Id = a.Attribute("id").Value,
+                                  Estado = a.Attribute("estado").Value
+                              });
+
+            foreach (var asiento in asientos)
+            {
+                string claseCss = "seat disponible"; // Estado por defecto
+
+                if (asiento.Estado == "ocupado")
+                    claseCss = "seat ocupado";
+                else if (asiento.Estado == "seleccionado")
+                    claseCss = "seat seleccionado";
+
+                LiteralControl seatDiv = new LiteralControl($"<div id='{asiento.Id}' class='{claseCss}'></div>");
+                AsientosContainer.Controls.Add(seatDiv);
+            }
+        }
+    }
+}
