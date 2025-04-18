b1361a635efda82c37e50bc0a754b8bfb6205a4f
Sebastian Mora <133081239+Sebas2203@users.noreply.github.com>
2025-04-18 16:51:17 -0600

    Creacion de capas datos, negocios. Coneccion a la base de datos para monstrar las peliculas

---
+++ Datos/PeliculasDatos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class PeliculasDatos
+    {
+        //atributos de la clase Peliculas
+        public int id { get; set; }
+        public string nombrePelicula { get; set; }
+        public string descripcion { get; set; }
+        public string urlImagen { get; set; }
+        public decimal precio { get; set; }
+        public string horario { get; set; }
+    }
+}
---
+++ Negocios/lnCine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    public class lnCine
+    {
+        //metodo que consulta articulos en la capa de datos  
+        public DataTable ObtenerPeliculas_Negocios()
+        {
+            try
+            {
+                //instanciar la capa de datos  
+                Datos.ldCine datos = new Datos.ldCine();
+
+                //se ejecuta la llamada  
+                var resultado = datos.ObtenerPelicula();
+
+                //si se require, se realiza una validacion de la info obtenida de la capa de datos  
+
+                //retorna el resultado  
+                return resultado;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
